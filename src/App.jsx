dce810482261dca5baff818d3d2c6d46d68232a9
Vimal <vm.21cs48@thdcihet.ac.in>
2024-09-05 18:03:55 +0530

    fixed few functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import { useState } from 'react'
-import Members from './components/Members'
-import Plans from './components/Plans'
-import ContactForm from './components/Contact_us'
-import MapComponent from './components/Geographical'
-import Header from './components/Header'
-import { Link,Router,Routes,Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Members from './components/Members';
+import Plans from './components/Plans';
+import ContactForm from './components/Contact_us';
+import MapComponent from './components/Geographical';
+import Header from './components/Header';
+
 function App() {
-  
   return (
-    <>
-   
-   <Header/>
-   <Routes>
+    <Router>
+      <Header />
+      <Routes>
         <Route path="/" element={<Plans />} />
         <Route path="/plans" element={<Plans />} />
         <Route path="/members" element={<Members />} />
         <Route path="/contact" element={<ContactForm />} />
-        <Route path="/connections" element={<MapComponent/>} />
+        <Route path="/connections" element={<MapComponent />} />
       </Routes>
-    </>
-   
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
